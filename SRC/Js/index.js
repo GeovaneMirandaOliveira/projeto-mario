70d48cbd568f541081ba6c3e1b76ca16324a746d
Geovane Miranda Oliveira <101004714+GeovaneMirandaOliveira@users.noreply.github.com>
2023-01-24 10:58:17 -0300

    Add files via upload

---
+++ SRC/Js/index.js
@@ -0,0 +1,20 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal() {
+    modal.classList.toggle("aberto");/*toggle verifica se tem mais de um elemento*/
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
